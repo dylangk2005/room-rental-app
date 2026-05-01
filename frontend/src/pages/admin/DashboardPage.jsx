c61891f38f78ef379006e138bc31a86c221521d5
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-05-01 19:55:59 +0700

    feat: setup React Router + create placeholder pages

---
+++ frontend/src/pages/admin/DashboardPage.jsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            Admin Dashboard Page
+        </div>
+    )
+}
+
+export default DashboardPage
---
+++ frontend/src/pages/posts/PostDetailPage.jsx
@@ -0,0 +1,7 @@
+const PostDetailPage = () => {
+    return (
+        <div>Post Detail Page</div>
+    )
+}
+
+export default PostDetailPage;

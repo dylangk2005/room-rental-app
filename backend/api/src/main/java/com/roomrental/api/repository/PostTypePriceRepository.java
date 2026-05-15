9a0cf230278d11ae08b4f4e7cb4bc24d1ac7796a
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-05-16 01:21:28 +0700

    feat(payment): add post payment service

--- backend/api/src/main/java/com/roomrental/api/repository/PostTypePriceRepository.java
+++ backend/api/src/main/java/com/roomrental/api/repository/PostTypePriceRepository.java
@@ -2,7 +2,10 @@ package com.roomrental.api.repository;
 
 import com.roomrental.api.entity.PostTypePrice;
 import com.roomrental.api.entity.PostTypePriceId;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-interface PostTypePriceRepository extends Repository<PostTypePrice, PostTypePriceId> {
+@Repository
+public interface PostTypePriceRepository extends JpaRepository<PostTypePrice, PostTypePriceId> {
+
 }

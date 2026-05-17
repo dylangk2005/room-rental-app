b0b515b3bee7a0b8edac27be553580c4d0d2dee8
dylangk2005 <phamdinhhai.dev@gmail.com>
2026-05-18 01:29:36 +0700

    feat(management): add query in paymentRepository for statistics

--- backend/api/src/main/java/com/roomrental/api/repository/PaymentRepository.java
+++ backend/api/src/main/java/com/roomrental/api/repository/PaymentRepository.java
@@ -4,15 +4,34 @@ import com.roomrental.api.entity.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
     Page<Payment> findByUserId(Integer userId, Pageable pageable);
+
     Optional<Payment> findTopByPostIdAndPaymentTypeOrderByCreatedAtDesc(
             Integer postId,
             Payment.PaymentType paymentType
     );
+
+    @Query("""
+    SELECT COALESCE(SUM(p.finalFee), 0)
+    FROM Payment p
+    WHERE p.paymentType IN :types
+      AND p.createdAt BETWEEN :from AND :to
+""")
+    BigDecimal sumFinalFeeByTypesAndCreatedAtBetween(
+            @Param("types") List<Payment.PaymentType> types,
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to
+    );
+
 }
